7b0c4dd5c2678051ce51ed0486555981a2fcfe7e
Elvis Feagin <47120736+ElvisFeagin@users.noreply.github.com>
2019-05-10 07:26:33 -0700

    2. Understanding RxJs - What are Streams

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    document.addEventListener('click', evt => {
+      console.log(evt);
+    });
+    let counter = 0;
+    setInterval(() => {
+      console.log(counter);
+      counter++;
+    }, 1000);
 
+    setTimeout(() => {
+      console.log('finished...');
+    }, 3000);
+  }
 }
