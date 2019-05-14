06fffd7388cfad7cf790d6db51b7ab8df89a575a
Elvis Feagin <47120736+ElvisFeagin@users.noreply.github.com>
2019-05-14 15:43:11 -0700

    6. RxJs 6: Learn How Observables Work Under the Hood, Build Your Own HTTP Observable

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { interval, timer, fromEvent } from 'rxjs';
+import { Observable, interval, timer, fromEvent, noop } from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -9,14 +9,22 @@ import { interval, timer, fromEvent } from 'rxjs';
 export class AboutComponent implements OnInit {
   constructor() {}
   ngOnInit() {
-    const interval$ = timer(3000, 1000);
-    const sub = interval$.subscribe(val => console.log('stream 1 => ' + val));
-    setTimeout(() => sub.unsubscribe(), 5000);
-    const click$ = fromEvent(document, 'click');
-    click$.subscribe(
-      evt => console.log(evt),
-      err => console.log(err),
-      () => console.log('completed')
+    const http$ = Observable.create(observer => {
+      fetch('/api/courses')
+        .then(response => {
+          return response.json();
+        })
+        .then(body => {
+          observer.next(body);
+          observer.complete();
+          observer.next();
+        })
+        .catch(err => {
+          observer.error(err);
+        });
+    });
+    http$.subscribe(courses => console.log(courses), noop, () =>
+      console.log('completed')
     );
   }
 }
