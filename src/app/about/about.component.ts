d3b75616e54dd7004e8668ae50700643755e2037
Elvis Feagin <47120736+ElvisFeagin@users.noreply.github.com>
2019-05-16 09:25:42 -0700

    8. RxJs 6: Building Components with RxJs - Imperative Design

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -10,11 +10,5 @@ import { createHttpObservable } from '../common/util';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-  ngOnInit() {
-    const http$ = createHttpObservable('/api/courses');
-    const courses$ = http$.pipe(map(res => Object.values(res['payload'])));
-    courses$.subscribe(courses => console.log(courses), noop, () =>
-      console.log('completed')
-    );
-  }
+  ngOnInit() {}
 }
